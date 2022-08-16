9ec72df85408cc7379d247d3257e01f0bdf402f7
Alexandra-Jade <iamalexjade@gmail.com>
2022-08-16 16:14:16 +0200

    Initial Push

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       Hello I'm a Dictionary
+      <div> <a href="#" className="btn btn-primary">Click Me</a></div>
+     
       </header>
     </div>
   );
