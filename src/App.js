928cfbe8bb4ae20f17a113e9390b21b8b597e267
Alexandra-Jade <iamalexjade@gmail.com>
2022-08-16 16:17:58 +0200

    Bootstrap Test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
       Hello I'm a Dictionary
-      <div> <a href="#" className="btn btn-primary">Click Me</a></div>
-     
+      <div>
+      <a href="https://visionary-sprite-04f6dd.netlify.app/" className="btn btn-primary">Click Me</a>
+      </div>
       </header>
     </div>
   );
