35e4a6283e3f9fd98032387fcee927bae311004d
Alexandra-Jade <iamalexjade@gmail.com>
2022-08-18 21:20:11 +0200

    Search Engine Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       Hello I'm a Dictionary
       <div>
       <a href="https://visionary-sprite-04f6dd.netlify.app/" className="btn btn-primary">Click Me</a>
       </div>
       </header>
+      </div>
+      <main className="text-center">
+        <Dictionary />
+      </main>
+      
+      <footer className="App-footer"><small>This project was coded by <a href="https://jadedcode.netlify.app/">Alexandra Jade Rodrigues</a> and is open sourced on <a href="https://github.com/Alexandra-Jade">GitHub</a> and hosted on <a href="https://visionary-sprite-04f6dd.netlify.app/">netlify</a>   </small>
+      </footer>
+   
     </div>
   );
 }
