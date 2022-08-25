1a1874e7634b7e9b147377c459d5cfee1a471d8a
Alexandra-Jade <iamalexjade@gmail.com>
2022-08-25 08:08:13 +0200

    Added alt attribute to Photos.js

--- src/App.js
+++ src/App.js
@@ -37,6 +37,8 @@ function App() {
         <Dictionary defaultKeyword="travel" />
       </main>
       
+
+      
       <footer className="App-footer"><small>This project was coded by <a href="https://jadedcode.netlify.app/">Alexandra Jade Rodrigues</a> and is open sourced on <a href="https://github.com/Alexandra-Jade">GitHub</a> and hosted on <a href="https://visionary-sprite-04f6dd.netlify.app/">netlify</a>   </small>
       </footer>
    
--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos (props) {
     return (
 <div className="col-4" key={index}>
     <a href={photo.src.original} target="_blank" rel="noreferrer">
-        <img src={photo.src.landscape} className="img-fluid" />
+        <img src={photo.src.landscape} className="img-fluid" alt={photo.alt}/>
         </a>
         </div>
         );
@@ -26,3 +26,4 @@ export default function Photos (props) {
     return null;
 }
 }
+
